e5d5bd419c69aa194d5a7d6b3f769b5a49f3a294
Mradul Sonare <tashutech410@gmail.com>
2026-05-11 01:09:07 +0530

    ui level cemplete

---
+++ src/main/java/RegisterServlet.java
@@ -0,0 +1,32 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@WebServlet("/RegisterServlet")
+
+public class RegisterServlet extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request,
+                          HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+
+        String name = request.getParameter("name");
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        String role = request.getParameter("role");
+
+        out.println("<h1>Registration Successful</h1>");
+
+        out.println("Name: " + name + "<br>");
+        out.println("Email: " + email + "<br>");
+        out.println("Role: " + role);
+    }
+}
